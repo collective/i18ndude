5673ae0c00499f7f93933e51cbf43af572709e5a
dtremea <dtremea@example.org>
2005-08-26 17:58:26 +0000

    Added better path handling. Uncovered a fail test in PTReader testcase. Added PYReader TestCase to demonstrate current broken behavior. Fixed sample pot encoding.
    svn path=/i18ndude/trunk/; revision=11397

---
+++ tests/input/test2.py
@@ -0,0 +1,19 @@
+# bla
+
+_ = context.translate
+
+zero = context.translate('Zero')
+one = context.translate('One', domain='testing')
+two = _('Two')
+
+out = context.translate('Out1', domain='running')
+#out = context.translate('Out2', domain='testing')
+
+if True:
+    three = context.translate(msgid='msgid_three',
+                              default='Three')
+
+four = context.translate(msgid='msgid_four',
+                         default='Four ${map}',
+                         {'map': zero})
+return four
