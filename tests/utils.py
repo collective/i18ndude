bf493d79821fbfc69cc9dfec85efff2378043702
Hanno Schlichting <hanno@hannosch.eu>
2006-05-12 01:23:38 +0000

    Fixed tests, so they can be run with the normal Zope testrunner.
    svn path=/i18ndude/trunk/; revision=23320

--- tests/utils.py
+++ tests/utils.py
@@ -2,5 +2,7 @@ import os.path
 from Globals import package_home
 
 GLOBALS = globals()
-
 PACKAGE_HOME = os.path.dirname(package_home(GLOBALS))
+
+if not PACKAGE_HOME.endswith('tests'):
+    PACKAGE_HOME = os.path.join(PACKAGE_HOME, 'tests')
