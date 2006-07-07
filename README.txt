f2753416dae3ec30557262ef359dd8e9291024ba
Hanno Schlichting <hanno@hannosch.eu>
2006-07-07 15:03:15 +0000

    Egg enabled i18ndude. The next release will be available as an egg. If you are in a development environment you might want to reinstall i18ndude by using 'python setup.py develop' instead of 'python setup.py install' now, so you don't have to do this whenever something changes in SVN :)
    svn path=/i18ndude/trunk/; revision=25864

--- README.txt
+++ README.txt
@@ -1,33 +1,31 @@
+i18ndude package readme
+=======================
+
+Overview
+--------
+
 Call i18ndude with the --help argument to see what it can do for you.
 
-==============
 Usage Examples
-==============
+--------------
 
--   Say you want i18ndude to scan for all page templates in directory 'skins/'
+  - Say you want i18ndude to scan for all page templates in directory 'skins/'
     and print the report to stdout:
 
     $ ./i18ndude find-untranslated `find skins/ -iregex '.*\..?pt$'`
 
--   If you wanted to synchronize your master pot-file (say 'plone.pot') with a
+  - If you wanted to synchronize your master pot-file (say 'plone.pot') with a
     language-specific version (say 'plone-eo.po'), you could go about it like
     this:
 
     $ ./i18ndude sync --pot plone.pot plone-eo.po
     
-    Note that, unless you provide the -s option here, i18ndude will append
-    to 'plone-eo.po' a report including all msgids that were added and all that
-    were removed. Delete that report when you're done.
-
-========
 Commands
-========
+--------
 
 See the output of i18ndude --help
 
-===========
 Development
-===========
+-----------
 
 i18ndude is being developed at http://plone.org/products/i18ndude
-
--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys, os
 
-# distutils setup script:
-# call it with: python setup.py install
+version = '2.1'
 
-from distutils.core import setup
-
-setup(author='Plone I18N team',
+setup(name='i18ndude',
+      version=version,
+      description="i18ndude performs various tasks related to ZPT's, Python Scripts and i18n.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='Plone i18n zpt',
+      author='Plone i18n team',
       author_email='plone-i18n@lists.sourceforge.net',
-      download_url='http://plone.org/products/i18ndude',
-      description='i18ndude performs various tasks related to Zope '
-      'products and i18n.',
+      url='http://plone.org/products/i18ndude',
       license='GPL',
-      name='i18ndude',
-      version=open('version.txt').read().strip(),
       package_dir = {'i18ndude':'.'},
       packages=['i18ndude'],
-      scripts=['i18ndude'])
+      #packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      scripts=['i18ndude'],
+      )
