f73a41ae79710608aab90118221225bf9e149b12
Maurits van Rees <maurits@vanrees.org>
2012-10-01 17:57:00 +0200

    Cleanup setup.py, point to github.

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
+import os
+import sys
+
 from setuptools import setup, find_packages
-import os, sys
 
 version = '3.2.3dev'
 
 install_requires = [
-  'zope.tal >= 3.5.2',
-  'zope.interface >= 3.3',
-  'zope.i18nmessageid >= 3.3',
-  'plone.i18n',
-  'ordereddict',
-]
+    'zope.tal >= 3.5.2',
+    'zope.interface >= 3.3',
+    'zope.i18nmessageid >= 3.3',
+    'plone.i18n',
+    'ordereddict',
+    ]
 
 if sys.version_info < (2, 5):
     install_requires.append('elementtree')
@@ -17,25 +19,25 @@ if sys.version_info < (2, 5):
 setup(name='i18ndude',
       version=version,
       description="i18ndude performs various tasks related to ZPT's, Python Scripts and i18n.",
-      long_description=open("README.txt").read() + "\n" + \
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=(open("README.txt").read() + "\n" +
+                        open(os.path.join("docs", "HISTORY.txt")).read()),
       classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Framework :: Plone',
-        'Framework :: Zope2',
-        'Framework :: Zope3',
-        'Environment :: Console',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Utilities',
-        ],
+          'Development Status :: 5 - Production/Stable',
+          'Framework :: Plone',
+          'Framework :: Zope2',
+          'Framework :: Zope3',
+          'Environment :: Console',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Utilities',
+          ],
       keywords='Plone i18n zpt',
       author='Daniel Nouri',
       author_email='plone-i18n@lists.sourceforge.net',
       maintainer='Vincent Fretin',
       maintainer_email='vincent.fretin@gmail.com',
-      url='http://pypi.python.org/pypi/i18ndude',
+      url='https://github.com/collective/i18ndude',
       license='GPL',
       package_dir={'': 'src'},
       packages=find_packages('src', exclude=['ez_setup']),
